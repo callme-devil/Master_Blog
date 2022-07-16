fb70b7480209275336b1a047c1d4fcd76e171d39
callme-devil <pransermi@gmail.com>
2022-07-16 17:40:36 +0430

    Add project files.

---
+++ MB.Application.Contracts/ArticleCategory/ArticleCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MB.Application.Contracts.ArticleCategory
+{
+    public class ArticleCategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string CreationDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ MB.Application/ArticleCategoryApplication.cs
@@ -0,0 +1,12 @@
+﻿using MB.Application.Contracts.ArticleCategory;
+
+namespace MB.Application
+{
+    public class ArticleCategoryApplication :IArticleCategoryApplication
+    {
+        public List<ArticleCategoryViewModel> List()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
