9522133e65ad03431f7759303a7f37dfbf2fda9d
callme-devil <pransermi@gmail.com>
2022-07-31 16:03:38 +0430

    refactor: UnitOfWork Configured  in CommentApplication (Begin & Commit)

--- MB.Application/CommentApplication.cs
+++ MB.Application/CommentApplication.cs
@@ -1,21 +1,26 @@
-﻿using MB.Application.Contracts.Comment;
+﻿using _01_Framework.Infrastructure.UnitOfWork;
+using MB.Application.Contracts.Comment;
 using MB.Domain.CommentAgg;
 
 namespace MB.Application
 {
     public class CommentApplication : ICommentApplication
     {
+        private readonly IUnitOfWork _unitOfWork;
         private readonly ICommentRepository _commentRepository;
 
-        public CommentApplication(ICommentRepository commentRepository)
+        public CommentApplication(ICommentRepository commentRepository, IUnitOfWork unitOfWork)
         {
             _commentRepository = commentRepository;
+            _unitOfWork = unitOfWork;
         }
 
         public void Add(AddComment command)
         {
+            _unitOfWork.BeginTran();
             var comment = new Comment(command.Name, command.Email, command.Message, command.ArticleId);
             _commentRepository.Create(comment);
+            _unitOfWork.CommitTran();
         }
 
         public List<CommentViewModel> GetList()
@@ -25,16 +30,18 @@ namespace MB.Application
 
         public void Confirm(int id)
         {
+            _unitOfWork.BeginTran();
             var comment = _commentRepository.Get(id);
             comment.Confirm();
-            //_commentRepository.Create();
+            _unitOfWork.CommitTran();
         }
 
         public void Cancel(int id)
         {
+            _unitOfWork.BeginTran();
             var comment = _commentRepository.Get(id);
             comment.Canceled();
-            //_commentRepository.Save();
+            _unitOfWork.CommitTran();
         }
     }
 }
