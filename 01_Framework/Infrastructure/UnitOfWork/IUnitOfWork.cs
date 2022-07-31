4662c5f641be6e8d652f6901399d36b2d622ef27
callme-devil <pransermi@gmail.com>
2022-07-31 15:53:30 +0430

    feat: UnitOfWork Implemented

---
+++ 01_Framework/Infrastructure/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace _01_Framework.Infrastructure.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        void BeginTran();
+
+        void CommitTran();
+
+        void RollBack();
+    }
+}
---
+++ 01_Framework/Infrastructure/UnitOfWork/UnitOfWorkEf.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _01_Framework.Infrastructure.UnitOfWork;
+
+public class UnitOfWorkEf : IUnitOfWork
+{
+    private readonly DbContext _context;
+
+    public UnitOfWorkEf(DbContext context)
+    {
+        _context = context;
+    }
+
+    public void BeginTran()
+    {
+        _context.Database.BeginTransaction();
+    }
+
+    public void CommitTran()
+    {
+        _context.Database.CommitTransaction();
+    }
+
+    public void RollBack()
+    {
+        _context.Database.RollbackTransaction();
+    }
+}
