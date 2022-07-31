690182277b9f60537933c01b9acef9aaf0b5eec0
callme-devil <pransermi@gmail.com>
2022-07-31 13:26:44 +0430

    feat: IRepository Filled

--- 01_Framework/Infrastructure/IRepository.cs
+++ 01_Framework/Infrastructure/IRepository.cs
@@ -1,7 +1,18 @@
-﻿namespace _01_Framework.Infrastructure
-{
-    public interface IRepository
-    {
+﻿using System.Linq.Expressions;
+using _01_Framework.Domain;
 
+namespace _01_Framework.Infrastructure
+{
+    public interface IRepository<in TKey,T> where T : DomainBase<TKey>
+    {
+        void Create(T entity);
+
+        T Get(TKey id);
+
+        List<T> GetList();
+
+        bool Exists(Expression<Func<bool, T>> expression);
+
+        
     }
 }
